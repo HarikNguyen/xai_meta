d3bd9dbd127b41160c0355f439344b8d781f36f9
HarikNguyen <hariknguyen.dev@gmail.com>
2026-02-09 21:16:45 +0700

    add papers

--- src/scripts/warm_up.py
+++ src/scripts/warm_up.py
@@ -54,8 +54,8 @@ def warm_up():
             "metatrain_iterations": metaval_iterations,
             "n_way": 5,
             "k_shot": 1,
-            "k_query": 15,
-            "meta_batch_size": 4,
+            "k_query": 1,
+            "meta_batch_size": 1, # Always use meta batch size of 1 when testing
             "shuffle": True,
         },
     )
@@ -76,8 +76,8 @@ def warm_up():
         "baselearner_args": baselearner_args,
         "optim_fn": torch.optim.Adam,
         "T": 5,
-        "T_val": 5,
-        "T_test": 5,
+        "T_val": 1, # Eval on at least one update f
+        "T_test": 10,
         "train_batch_size": 1,
         "test_batch_size": 4,
         "lr": 0.001,
