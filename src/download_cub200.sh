3750da358572441ddfd095d24f44621194118e3b
HarikNguyen <hariknguyen.dev@gmail.com>
2026-06-04 15:23:09 +0700

    add cub

--- src/download_cub200.sh
+++ src/download_cub200.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-wget -O miniImagenet.zip "https://huggingface.co/datasets/hariknguyen2419/FSL_datasets/resolve/main/CUB_200_2011.zip?download=true"
+wget -O cub200.zip "https://huggingface.co/datasets/hariknguyen2419/FSL_datasets/resolve/main/CUB_200_2011.zip?download=true"
 unzip -q cup200.zip
 rm -rf cub200.zip
 
