b5e4ba3d562312a0ea9c295ae8ffe8b5f075a390
HarikNguyen <hariknguyen.dev@gmail.com>
2026-07-02 04:28:49 +0700

    eval explaination

--- src/scripts/check_explain/sanity_support_set.py
+++ src/scripts/check_explain/sanity_support_set.py
@@ -28,6 +28,7 @@ def permute_label(sup_y, flip_ratio=0.6):
             per_val = sup_y[per_idx]
             print(per_val)
             print(orig_val)
+            print()
             if not torch.equal(per_val, orig_val):
                 sup_y_np[idx] = per_val
                 break
