2e99bcc8448f1a613d776d213b24cc5fa95aab3b
HarikNguyen <hariknguyen.dev@gmail.com>
2026-07-03 13:35:23 +0700

    eval explaination

--- src/scripts/check_explain/sanity_support_set.py
+++ src/scripts/check_explain/sanity_support_set.py
@@ -202,9 +202,6 @@ def check_on_mixed_task(explainer, source_task, another_task, T):
     (sup_x, sup_y), (que_x, que_y) = source_task
     (ood_sup_x, ood_sup_y), (ood_que_x, ood_que_y) = mix_set(source_task, another_task, num_mixed_classes=2)
     
-    print(ood_sup_x.shape, ood_sup_y.shape, ood_que_x.shape, ood_que_y.shape)
-    print(ood_
-
     _, orig_saliency_map = explainer.interpret(sup_x, sup_y, que_x, que_y, T)
     _, mixed_saliency_map = explainer.interpret(ood_sup_x, ood_sup_y, ood_que_x, ood_que_y, T)
 
