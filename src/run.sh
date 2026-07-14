0d34f34f2631262bb04396618b1762ec4d94968f
HarikNguyen <hariknguyen.dev@gmail.com>
2026-07-14 07:41:58 +0700

    update run

--- src/run.sh
+++ src/run.sh
@@ -8,16 +8,16 @@
 # Check n Explain
 
 echo "Check n Explain - Mini 2 CUB"
-python main.py --config ./configs/mini2cub.yaml --mode test --use_last
-python main.py --config ./configs/mini2cub.yaml --mode explain --use_last
+python main.py --config ./configs/mini2cub.yaml --checkpoint_dir MAML_mini2cub --log_dir mini2cub --mode test --use_last
+python main.py --config ./configs/mini2cub.yaml --checkpoint_dir MAML_mini2cub --log_dir mini2cub --mode explain --use_last
 
 echo "Check n Explain - Tiered 2 CUB"
-python main.py --config ./configs/tiered2cub.yaml --mode test --use_last
-python main.py --config ./configs/tiered2cub.yaml --mode explain --use_last
+python main.py --config ./configs/tiered2cub.yaml --checkpoint_dir MAML_tiered2cub --log_dir tiered2cub --mode test --use_last
+python main.py --config ./configs/tiered2cub.yaml --checkpoint_dir MAML_tiered2cub --log_dir tiered2cub --mode explain --use_last
 
 echo "Check n Explain - Tiered"
-python main.py --config ./configs/tiered.yaml --mode test --use_last
-python main.py --config ./configs/tiered.yaml --mode explain --use_last
+python main.py --config ./configs/tiered.yaml --checkpoint_dir MAML_tiered_tiered --log_dir tiered --mode test --use_last
+python main.py --config ./configs/tiered.yaml --checkpoint_dir MAML_tiered_tiered --log_dir tiered --mode explain --use_last
 
 # Check Explain Method (biADT + sanity check params + sanity support - hard/noise/ood)
 echo "Check Explain Method"
